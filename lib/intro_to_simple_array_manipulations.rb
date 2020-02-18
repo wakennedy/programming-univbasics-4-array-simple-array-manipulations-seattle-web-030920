e713c74dc4f60688be2f7ee8ec2bc9f54c6fa609
William Kennedy <alexkenned@gmail.com>
2020-02-18 18:45:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,5 +26,9 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array)
-  array.insert(2)
+def using_insert(array, element)
+  array.insert(2, element)
+end
+
+def using_uniq(array)
+  
